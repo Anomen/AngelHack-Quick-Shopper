699936d2e35f9e2b74c518790e1d4af945bf127c
maneeshmoota <maneesh.moota@gmail.com>
2012-11-17 14:33:01 -0500

    index.html modified, added login collection, model, view, template

--- www/router.js
+++ www/router.js
@@ -1,50 +1,59 @@
-define(['hackathon'], function() {
-    var Router = Hackathon.Router.extend({
-        routes: {
-            ''          : 'locations' , 
-            'locations' : 'locations' , 
-            'badges'    : 'badges'    , 
-            'news'      : 'news'      , 
-            'events'    : 'events'
-        },
-        initialize: function(){
-            t("inside initialize [router.js]");
+define(['hackathon'], function () {
+	var Router = Hackathon.Router.extend({
+		routes    :{
+			''      :'login',
+			'login' :'login',
+			'badges':'badges',
+			'news'  :'news',
+			'events':'events'
+		},
+		initialize:function () {
+			t("inside initialize [router.js]");
 
-            window.mapsFirstLoad = true;
-        },
-        locations: function() {
-            require(['views/locationsView'], function (LocationsView) {
-                t("inside initialize #locations");
-                if (typeof App.views.locationsView === "undefined")
-                    App.views.locationsView = new LocationsView;
-                App.views.locationsView.render();
-            });
-        },
-        badges: function() {
-            require(['views/badgesView'], function (BadgesView) {
-                t("inside initialize #badges");
-                if (typeof App.views.badgesView === "undefined")
-                    App.views.badgesView = new BadgesView;
-                App.views.badgesView.render();
-            });
-        },
-        news: function() {
-            require(['views/newsView'], function (NewsView) {
-                t("inside initialize #news");
-                if (typeof App.views.newsView === "undefined")
-                    App.views.newsView = new NewsView;
-                App.views.newsView.render();
-            });
-        },
-        events: function() {
-            require(['views/eventsView'], function (EventsView) {
-                t("inside initialize #events");
-                if (typeof App.views.eventsView === "undefined")
-                    App.views.eventsView = new EventsView;
-                App.views.eventsView.render();
-            });
-        }
-    });
+			window.mapsFirstLoad = true;
+		},
+		login     :function () {
+			require(['views/loginView'], function (LoginView) {
+				t("inside #login");
+				if (typeof App.views.loginView === "undefined") {
+					App.views.loginView = new LoginView;
+				}
+				App.views.loginView.render();
+			});
+		},
+		locations :function () {
+			require(['views/locationsView'], function (LocationsView) {
+				t("inside initialize #locations");
+				if (typeof App.views.locationsView === "undefined")
+					App.views.locationsView = new LocationsView;
+				App.views.locationsView.render();
+			});
+		},
+		badges    :function () {
+			require(['views/badgesView'], function (BadgesView) {
+				t("inside initialize #badges");
+				if (typeof App.views.badgesView === "undefined")
+					App.views.badgesView = new BadgesView;
+				App.views.badgesView.render();
+			});
+		},
+		news      :function () {
+			require(['views/newsView'], function (NewsView) {
+				t("inside initialize #news");
+				if (typeof App.views.newsView === "undefined")
+					App.views.newsView = new NewsView;
+				App.views.newsView.render();
+			});
+		},
+		events    :function () {
+			require(['views/eventsView'], function (EventsView) {
+				t("inside initialize #events");
+				if (typeof App.views.eventsView === "undefined")
+					App.views.eventsView = new EventsView;
+				App.views.eventsView.render();
+			});
+		}
+	});
 
-    return Router;
+	return Router;
 });
