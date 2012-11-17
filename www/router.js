da0ffeca54f65d5537b60e933dcb11cf2cfd2318
oeeckhoutte <olivier.eeckhoutte@gmail.com>
2012-11-17 14:18:39 -0500

    Node server creation to serve files + Routes created in client side

--- www/router.js
+++ www/router.js
@@ -1,11 +1,14 @@
 define(['hackathon'], function() {
     var Router = Hackathon.Router.extend({
         routes: {
-            ''          : 'locations' , 
-            'locations' : 'locations' , 
-            'badges'    : 'badges'    , 
-            'news'      : 'news'      , 
-            'events'    : 'events'
+            ''              : 'locations' , 
+            'locations'     : 'locations' , 
+            'badges'        : 'badges'    , 
+            'news'          : 'news'      , 
+            'events'        : 'events'    ,
+            'shopMap'       : 'shopMap'   ,
+            'shopMapList'   : 'shopMapList'
+
         },
         initialize: function(){
             t("inside initialize [router.js]");
@@ -43,7 +46,27 @@ define(['hackathon'], function() {
                     App.views.eventsView = new EventsView;
                 App.views.eventsView.render();
             });
+        },
+        shopMap: function() {
+            require(['views/shopMapView'], function (EventsView) {
+                t("inside shopMap [router.js]");
+                if (typeof App.views.eventsView === "undefined") {
+                    App.views.shopMapView = new ShopMapView;
+                }
+                App.views.shopMapView.render();
+            });
+        },
+        shopMapList: function() {
+            require(['views/shopMapListView'], function (EventsView) {
+                t("inside shopMapList [router.js]");
+                if (typeof App.views.eventsView === "undefined") {
+                    App.views.shopMapListView = new ShopMapListView;
+                }
+                App.views.shopMapListView.render();
+            });
         }
+
+
     });
 
     return Router;
